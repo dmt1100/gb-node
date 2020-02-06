e810dbb9342c5f5bfeeb62d1540bc659b530b9cb
bantsov-dmitriy <bantsov@gmail.com>
2020-02-06 23:35:40 +0300

    include colors.js and edit file index.js

--- lesson1/index.js
+++ lesson1/index.js
@@ -5,6 +5,8 @@ console.log('Hello Node.js!');
 
 //import-export - ES6+, require - CommonJS
 const ansi = require('ansi');
+//подключаем colors
+const colors = require('colors');
 
 //Создаем курсор
 const cursor = ansi(process.stdout);
@@ -25,3 +27,18 @@ cursor
 .reset()
 .bg.reset()
 .write('\n');
+
+//используем библиотеку colors
+console.log('\tHello world!'.rainbow);
+console.log('\t\tHello world!'.underline.green);
+colors.setTheme({
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+  });
+console.log('\tHello world!'.debug);
+
+//проверка по умолчанию
+console.log('Hello Node.js');
